051372f589572bd27b6e6b5c7f5e1e8c91de178a
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-02-24 22:23:02 -0300

    feat(database): replace mysql by postgres instead

--- src/core/database/database.service.ts
+++ src/core/database/database.service.ts
@@ -7,8 +7,9 @@ export class DatabaseService implements TypeOrmOptionsFactory {
   constructor(private readonly environmentService: EnvironmentService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
-    const sharedTypeOrmModuleOptions: TypeOrmModuleOptions = {
-      type: 'mysql',
+    return {
+      type: 'postgres',
+      host: this.environmentService.getEnvironmentValue('DB_HOST'),
       port: this.environmentService.getEnvironmentValue('DB_PORT'),
       username: this.environmentService.getEnvironmentValue('DB_USERNAME'),
       password: this.environmentService.getEnvironmentValue('DB_PASSWORD'),
@@ -17,18 +18,6 @@ export class DatabaseService implements TypeOrmOptionsFactory {
       migrations: ['dist/**/migrations/*.js'],
       migrationsTableName: 'migrations_typeorm',
       migrationsRun: true,
-    };
-
-    if (this.environmentService.isProd())
-      return {
-        ...sharedTypeOrmModuleOptions,
-        socketPath:
-          this.environmentService.getEnvironmentValue('DB_SOCKET_PATH'),
-      };
-
-    return {
-      ...sharedTypeOrmModuleOptions,
-      host: this.environmentService.getEnvironmentValue('DB_HOST'),
     };
   }
 }
