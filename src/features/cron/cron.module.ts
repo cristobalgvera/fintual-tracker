3936c2eba5222d912c8bc089fd63005a1148fd1e
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-04-04 18:04:30 -0400

    refactor(cron): add dynamic recognition of schedules and time zone

--- src/features/cron/cron.module.ts
+++ src/features/cron/cron.module.ts
@@ -1,11 +1,11 @@
-import { GoalsModule } from '@features/goals';
-import { TrackingModule } from '@features/tracking';
+import { EnvironmentModule } from '@core/environment';
+import { FollowUpModule } from '@features/follow-up';
 import { Logger, Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { CronService } from './cron.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), GoalsModule, TrackingModule],
+  imports: [ScheduleModule.forRoot(), EnvironmentModule, FollowUpModule],
   providers: [
     CronService,
     {
